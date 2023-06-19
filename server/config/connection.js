0533fb5ecdb5511490a40a96062861aeb91e7218
Leon Forsythe <lvforsythe95@gmail.com>
2023-06-19 10:16:34 -0400

    setup mongodb connection function

---
+++ server/config/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/totk_recipes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+modoule.exports = mongoose.connection
