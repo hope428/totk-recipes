cb52a1378105082f455205d29faa7dd5280842e2
Leon Forsythe <lvforsythe95@gmail.com>
2023-06-22 18:52:29 -0400

    refactored search controller

--- server/controllers/recipe-controllers.js
+++ server/controllers/recipe-controllers.js
@@ -4,26 +4,25 @@ module.exports = {
   async getAllRecipes(req, res) {
     try {
       const allRecipes = await Recipe.find({});
-      if(allRecipes.length < 1){
-        return res.status(200).json("No recipes found")
+      if (allRecipes.length < 1) {
+        return res.status(200).json("No recipes found");
       }
       return res.status(200).json(allRecipes);
     } catch (error) {
-        return res.status(400).json("Error: No recipes found")
+      return res.status(400).json("Error: No recipes found");
     }
   },
 
-  async getSearchedRecipes({params}, res) {
-
+  async getSearchedRecipes({ params }, res) {
     try {
-        const searchResults = await Recipe.find({ingredients: params.query})
-        return res.status(200).json(searchResults)
+      const searchResults = await Recipe.find({ ingredients: params.query });
+      return res.status(200).json(searchResults);
     } catch (err) {
-        return res.status(400).json(err)
+      return res.status(400).json(err);
     }
   },
 
   async getSingleRecipe(req, res) {
-    return res.status(202).json("Single recipe data goes here")
+    return res.status(202).json("Single recipe data goes here");
   },
 };
