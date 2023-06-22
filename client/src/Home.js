07868a93d8d8ac43cabd54d0cdc00c2296b7abd0
Leon Forsythe <lvforsythe95@gmail.com>
2023-06-22 18:49:49 -0400

    added swiper js for better results display

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function Home() {
   const [results, setResults] = React.useState([]);
@@ -21,16 +23,44 @@ export default function Home() {
     <div className="container">
       <Header />
 
-      <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
         {results.length > 0 ? (
-          results.map((res) => <img src={res.fullImage}/>)
+          results.map((res) => (
+            <SwiperSlide>
+              <div className="recipe-card">
+                <p>{res.name}</p>
+                <img alt={res.name} src={res.fullImage} />
+              </div>
+            </SwiperSlide>
+          ))
         ) : (
           <div className="search-field">
             <input id="query" type="text" placeholder="Search an ingredient" />
             <button onClick={handleSearch}>Search</button>
           </div>
         )}
-      </div>
+      </Swiper>
+
+      {/* <div className="results-container">
+        {results.length > 0 ? (
+          results.map((res) => (
+            <div className="recipe-card">
+              <p>{res.name}</p>
+              <img alt={res.name} src={res.fullImage} />
+            </div>
+          ))
+        ) : (
+          <div className="search-field">
+            <input id="query" type="text" placeholder="Search an ingredient" />
+            <button onClick={handleSearch}>Search</button>
+          </div>
+        )}
+      </div> */}
     </div>
   );
 }
