23e869fcafaa880d44a8bca3c6dcc1586fa482c5
Leon Forsythe <lvforsythe95@gmail.com>
2023-12-05 18:46:02 -0500

    created a function to fetch favorites by ID from the favorites array

--- client/src/Saved.js
+++ client/src/Saved.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Header from "./components/Header";
+import RecipeCard from "./components/RecipeCard";
 
 export default function Saved(){
+
+    const [favorites, setFavorites] = React.useState([]);
+
+    const ids = ["649a2b2e5f511166d07cae40", "649a2b2e5f511166d07cae41", "649a2b2e5f511166d07cae43"]
+
+    const getFavorites = () => {
+        ids.map((id) => {
+            fetch(`api/recipes/id/${id}`, {
+                method: "GET",
+                headers: {"Content-Type": "application/json"},
+            })
+            .then(res => res.json())
+            .then(data => setFavorites((prevFavorites) => (
+                [...prevFavorites, data]
+                )))
+        })
+    }
+    
+
     return (
         <>
         <Header />
         <h1>Saved Recipes:</h1>
+        {favorites.map(favorite => (
+            <h2 key={favorite.id}>{favorite.name}</h2>
+        ))}
         </>
     )
 }
